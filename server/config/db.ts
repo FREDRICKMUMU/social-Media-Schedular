111e0fe95de4dca5a4f4ca66fdea6fc68050a7bf
Fred Munyao <fredrickmumu2@gmail.com>
2026-06-17 14:35:54 +0300

    created API for social account creations

--- server/config/db.ts
+++ server/config/db.ts
@@ -5,12 +5,11 @@ const connectDB = async () => {
         mongoose.connection.on("connected", async () => {
             console.log('MongoDB connected')
         });
-        await mongoose.connect(process.env.MONGODB_URI!)
+        await mongoose.connect(process.env.MONGO_URI!) 
     } catch (error: any){
         console.error(error)
         process.exit(1)
     }
 }
-
 
 export default connectDB;
--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import socialAuthRouter from "./routes/socialAuthRoutes.js";
 
 const app = express();
 
@@ -17,6 +19,10 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use("/api/auth", authRouter)
+app.use("/api/oauth", socialAuthRouter)
+
 
 //GLOBAL ERROR HANDLER
 app.use((err: any, _req: Request, res: Response, _next: NextFunction)=>{
